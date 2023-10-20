8f509ff05b2bbb80f5c873c548db4c1cf04d27a4
Pao-Sheng Wang <paooap.oappao@gmail.com>
2023-10-21 00:32:51 +0900

    reset hello world program

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,3 @@
 fn main() {
     println!("Hello, world!");
-    println!("testing");
-
-    // let (x, y);
-
-    // (x, ..) = 5;
-    // (.., y) = 5;
-
-    // println!("x = {}, y = {}", x, y);
-    test();
-
-    let mut arr = [1, 5, 3, 2, 4];
-    bubble_sort(&mut arr);
-}
-
-fn test() {
-    println!("this it");
-}
-
-// create a function to do bubble sort on a integer array
-// and don't return anything but print the sorted array
-fn bubble_sort(arr: &mut [i32]) {
-    let mut n = arr.len();
-    let mut swapped = true;
-
-    while swapped {
-        swapped = false;
-        for i in 1..n {
-            if arr[i - 1] > arr[i] {
-                arr.swap(i - 1, i);
-                swapped = true;
-            }
-        }
-        n = n - 1;
-    }
-
-    println!("sorted array: {:?}", arr);
 }
