1ad61024deb4dd0b958bb77a56fb8f8ab316b9cb
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-02-05 22:37:39 +0800

    Different kinds of rust comments

---
+++ src/comment.rs
@@ -0,0 +1,20 @@
+// this is a line comment
+
+/*
+ * This is a block comment
+ * It can span multiple lines
+ */
+
+/// This is a documentation comment
+/// It can be used to generate documentation
+/// for the code
+/// ```
+/// let x = 5;
+/// let y = 6;
+/// ```
+
+
+//! This is a documentation comment as well, to comment containing items
+//! (e.g. crates, modules, functions). instead of the items following it.
+//! Commonly used inside the crate root file (lib.rs) or modules root file (mod.rs)
+//!
