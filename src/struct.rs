49dc4f0f1f216440aa716fa7e1bf83051efeccc8
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-02-13 03:16:10 +0800

    Using the Field Init Shorthand

--- src/struct.rs
+++ src/struct.rs
@@ -6,6 +6,22 @@ struct User {
     active: bool,
 }
 
+fn build_user(email: String, username: String) -> User {
+    // User {
+    //     email: email,
+    //     username: username,
+    //     active: true,
+    //     sign_in_count: 0,
+    // }
+    // if the parameter name is the same as the struct field name, we can use the field init shorthand
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 0,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
