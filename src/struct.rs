f5af8e65404658a3e45b62209e333139d590d1f2
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-02-13 02:57:20 +0800

    Add the fields in Struct

--- src/struct.rs
+++ src/struct.rs
@@ -1,4 +1,10 @@
-struct User {}
+#[derive(Debug)]
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
 
 #[cfg(test)]
 mod tests {
@@ -6,6 +12,17 @@ mod tests {
 
     #[test]
     fn test_struct() {
-        let user = User {};
+        let mut user = User {
+            // don't need to follow the order of the struct
+            sign_in_count: 0,
+            active: false,
+            username: String::from("user"),
+            email: String::from("user@gmail.com"),
+        };
+
+        println!("username: {:?}", user);
+
+        // if the struct is mutable, we can change the value of the struct
+        user.username = String::from("user2");
     }
 }
