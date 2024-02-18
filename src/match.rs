a8586a9acb2053d0e5f926493d57a2715fbb7474
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-02-18 22:57:33 +0800

    Matches Are Exhaustive

--- src/match.rs
+++ src/match.rs
@@ -29,6 +29,7 @@ fn value_in_cents(coin: Coin) -> u8 {
 }
 
 fn plus_one(x: Option<i32>) -> Option<i32> {
+    // match is exhaustive, so we need to handle all the cases to avoid a compile error
     match x {
         None => None,
         Some(i) => Some(i + 1)
