9f8f66c6c1f0d145d9ec31359406cdcc4cbb3883
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-02-26 22:50:04 +0800

    Providing New Names with the as Keyword

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -49,6 +49,19 @@ fn function2() -> io::Result<()> {
     Ok(())
 }
 
+mod fun {
+    use std::fmt::Result;
+    // we can use `as` keyword to rename the imported item
+    use std::io::Result as IoResult;
+
+    fn function1() -> Result {
+        Ok(())
+    }
+
+    fn function2() -> IoResult<()> {
+        Ok(())
+    }
+}
 
 fn deliver_order() {}
 
