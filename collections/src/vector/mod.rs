3b00726fbf6252192811d59047e8881a29421cff
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-07 23:10:19 +0800

    Reading Elements of Vectors

---
+++ collections/src/vector/mod.rs
@@ -0,0 +1,40 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_vector() {
+        let mut v: Vec<i32> = Vec::new();
+        v.push(1);
+        v.push(2);
+        v.push(3);
+        v.push(4);
+        v.push(5);
+        println!("{:?}", v);
+        let third: &i32 = &v[2];
+        println!("The third element is {}", third);
+
+        // using get method to access elements in vector to avoid panic
+        // when index is out of range. and it is more common to use get method
+        match v.get(2) {
+            Some(third) => println!("The third element is {}", third),
+            None => println!("There is no third element."),
+        }
+    }
+
+    #[test]
+    fn test_vector_macro() {
+        let v = vec![1, 2, 3, 4, 5];
+        println!("{:?}", v);
+    }
+
+    #[test]
+    fn test_vector_borrow_check() {
+        let mut v = vec![1, 2, 3, 4, 5];
+        let first = &v[0];
+        // v.push(6); // error[E0502]: cannot borrow `v` as mutable because it is also borrowed as immutable
+        // this error is due to the way vector works, when we push a new element into the vector, it might
+        // need to allocate new memory and copy the old elements to the new space, if that happens,
+        // the reference to the first element would be pointing to deallocated memory.
+        println!("The first element is: {}", first);
+    }
+
+}
