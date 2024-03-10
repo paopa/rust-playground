9b6fa1a1f72d4066a7e215cf8d3131bd7b538d48
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-10 21:11:29 +0800

    Indexing into Strings

--- collections/src/string/mod.rs
+++ collections/src/string/mod.rs
@@ -60,4 +60,39 @@ mod tests {
         let s = format!("{s1}-{s2}-{s3}");
         assert_eq!(s, "tic-tac-toe");
     }
+
+    #[test]
+    fn test_index_string() {
+        let hello = "Здравствуйте";
+        let answer = &hello.as_bytes()[0];
+        // if you run the code below:
+        //
+        // let h = hello[0];
+        //
+        // you will get an error because Rust strings don't support indexing.
+        // But why? The reason is that Rust string is a wrapper over a Vec<u8>.
+        // so, when you try to get the ownership of an element in the string, it will occur an error.
+        assert_eq!(*answer, 208);
+    }
+
+    #[test]
+    fn test_bytes_scalar_value_grapheme() {
+        let s = "नमस्ते";
+
+        let bytes = s.as_bytes(); //[224, 164, 168, 224, 164, 174, 224, 164, 184, 224, 165, 141, 224, 164, 164, 224, 165, 135]
+        println!("{:?}", bytes);
+        assert_eq!(bytes.len(), 18);
+
+        let scalar_values = s.chars(); // ['न', 'म', 'स', '्', 'त', 'े']
+        println!("{:?}", scalar_values);
+        assert_eq!(scalar_values.count(), 6);
+
+        // There are six char values here, but the fourth and sixth are not letters.
+        // they're diacritics that don't make sense on their own. Finally, if we look at them as grapheme clusters,
+        // we'd get what a person would call the four letters that make up the Hindi word: ["न", "म", "स्", "ते"]
+        //
+        // Rust provides different ways of interpreting the raw string data that computers store
+        // so that each program can choose the interpretation it needs, no matter what human language the data is in.
+
+    }
 }
