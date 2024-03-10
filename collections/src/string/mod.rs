b2f2b3142f01ad443423cd2673cdda969fce60ff
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-10 20:17:06 +0800

    Appending to a String with push_str and push

--- collections/src/string/mod.rs
+++ collections/src/string/mod.rs
@@ -18,4 +18,20 @@ mod tests {
         let s = String::from("hello");
         assert_eq!(s, "hello");
     }
+    #[test]
+    fn test_update_string() {
+        let mut s = String::from("foo");
+        s.push_str("bar");
+        assert_eq!(s, "foobar");
+
+        let mut s1 = String::from("foo");
+        let s2 = "bar";
+        s1.push_str(s2);
+        assert_eq!(s1, "foobar");
+        assert_eq!(s2, "bar");
+
+        let mut s = String::from("lo");
+        s.push('l');
+        assert_eq!(s, "lol");
+    }
 }
