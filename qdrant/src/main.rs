ce9343378d2b8cca46a472bcf6e3ea2a0fa888e9
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-05-31 22:55:29 +0800

    exper: qdrant client

--- qdrant/src/main.rs
+++ qdrant/src/main.rs
@@ -1,23 +1,34 @@
-use anyhow::Result;
-use qdrant_client::client::QdrantClient;
-use qdrant_client::qdrant::{vectors_config::Config, VectorParams, VectorsConfig};
-use qdrant_client::qdrant::{CreateCollection, Distance};
+use qdrant_client::{
+    client::QdrantClient,
+    qdrant::{vectors_config::Config, CreateCollection, Distance, VectorParams, VectorsConfig},
+};
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = QdrantClient::from_url("http://localhost:6334").build()?;
+
+    let collections_list = client.list_collections().await?;
+    dbg!(collections_list);
+
+    let collection_name = "test";
+    client.delete_collection(collection_name).await?;
 
     client
         .create_collection(&CreateCollection {
-            collection_name: "test_collection".to_string(),
+            collection_name: collection_name.into(),
             vectors_config: Some(VectorsConfig {
                 config: Some(Config::Params(VectorParams {
-                    size: 4,
-                    distance: Distance::Dot.into(),
+                    size: 10,
+                    distance: Distance::Cosine.into(),
                     ..Default::default()
                 })),
             }),
             ..Default::default()
         })
         .await?;
+
+    let collections_list = client.list_collections().await?;
+    dbg!(collections_list);
+
+    Ok(())
 }
