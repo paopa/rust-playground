b0ea11273acaa28e6773d346a362c51644dd589a
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-06-05 21:13:22 +0800

    qdrant: quick started example

--- qdrant/src/main.rs
+++ qdrant/src/main.rs
@@ -1,7 +1,11 @@
 use qdrant_client::{
-    client::QdrantClient,
-    qdrant::{vectors_config::Config, CreateCollection, Distance, VectorParams, VectorsConfig},
+    client::{Payload, QdrantClient},
+    qdrant::{
+        vectors_config::Config, Condition, CreateCollection, Distance, Filter, PointStruct,
+        SearchPoints, VectorParams, VectorsConfig,
+    },
 };
+use serde_json::json;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -30,5 +34,45 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let collections_list = client.list_collections().await?;
     dbg!(collections_list);
 
+    let payload: Payload = json!(
+        {
+            "foo": "Bar",
+            "bar": 12,
+            "baz": {
+                "qux": "quux"
+            }
+        }
+    )
+    .try_into()
+    .unwrap();
+
+    let points = vec![PointStruct::new(0, vec![12.; 10], payload)];
+    client
+        .upsert_points_blocking(collection_name, None, points, None)
+        .await?;
+
+    let search_result = client
+        .search_points(&SearchPoints {
+            collection_name: collection_name.into(),
+            vector: vec![11.; 10],
+            filter: Some(Filter::all([Condition::matches("bar", 12)])),
+            limit: 10,
+            with_payload: Some(true.into()),
+            ..Default::default()
+        })
+        .await?;
+    dbg!(&search_result);
+
+    let found_point = search_result.result.into_iter().next().unwrap();
+    let mut payload = found_point.payload;
+
+    let baz_payload = payload.remove("baz").unwrap().into_json();
+    println!("baz: {}", baz_payload);
+
+    let bar_payload = payload.remove("bar").unwrap().into_json();
+    println!("bar: {}", bar_payload);
+
+    let foo_payload = payload.remove("foo").unwrap().into_json();
+    println!("foo: {}", foo_payload);
     Ok(())
 }
